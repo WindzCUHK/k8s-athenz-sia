edd99230fa4cad5dbce7d1787f7a57e74e96cc14
Windz <WindzCUHK@users.noreply.github.com>
2023-05-23 02:59:50 +0900

    test merge (#2)
    * draft
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * fix env
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * multi-line
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * test false
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * test
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * fix
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * code fix
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * test push
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * push 2
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * push 3
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * push 3
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * fix binary version
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * use cache
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * draft
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * fix go test
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * no cache
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * debug
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * d 2
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * d 3
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * remove GOPATH in make
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * test
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * no push
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * push true
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * add status
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * fix intent
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * no tag
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * fix permission
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * test 2
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * ddd
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * fix cache, debug
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * ddd 2
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * use ENV
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * remove flag
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * fix flag
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * fix
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * test permission
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * refactor
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * fix platform
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * fix env
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * fix output
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * fix default
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * fix SHA, status
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * docker cache, fix debug msg
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * test RPM
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * add input output
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * fix jq
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * use env
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * test split
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * env ???
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * ttt
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * test job.env
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * test output
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * set json
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * json array
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * ttt
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * fix
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * test 4 platform
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    * final test
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>
    
    ---------
    
    Signed-off-by: wfan <wfan@yahoo-corp.jp>

--- cmd/athenz-sia/main.go
+++ cmd/athenz-sia/main.go
@@ -147,8 +147,8 @@ func parseFlags(program string, args []string) (*identity.IdentityConfig, error)
 		pollInterval = util.DefaultPollInterval
 	}
 	pollTokenInterval := tri
-	if pollTokenInterval > 4*time.Hour {
-		pollTokenInterval = 4 * time.Hour
+	if pollTokenInterval > identity.DEFAULT_POLL_TOKEN_INTERVAL {
+		pollTokenInterval = identity.DEFAULT_POLL_TOKEN_INTERVAL
 	}
 	reloader, err := util.NewCertReloader(util.ReloadConfig{
 		KeyFile:      keyFile,
@@ -213,7 +213,7 @@ func parseFlags(program string, args []string) (*identity.IdentityConfig, error)
 		RoleCertFilenameDelimiter: roleCertFilenameDelimiter,
 		RoleAuthHeader:            roleAuthHeader,
 		TokenType:                 tokenType,
-		TokenRefresh:              tri,
+		TokenRefresh:              pollTokenInterval,
 		TokenServerAddr:           tokenServerAddr,
 		TokenDir:                  tokenDir,
 		MetricsServerAddr:         metricsServerAddr,
